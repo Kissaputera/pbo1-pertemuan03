74d2241d60de985427ce7d6c55263522555b9433
Kissaputera <mkissaputera@gmail.com>
2023-05-04 21:55:59 +0800

    Update OperasiString.java

--- pbo1-ptm03/src/main/java/string/OperasiString.java
+++ pbo1-ptm03/src/main/java/string/OperasiString.java
@@ -1,13 +1,10 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package string;
 
-/**
- *
- * @author ACER
- */
 public class OperasiString {
+    public static void main(String[] args) {
+        String kota = "Yogyakarta";
+        
+        System.out.println(kota);
+    }
     
 }
